4e3bd4141ceaddf5e1820c5a955dcce939b39ce2
Saichandu Goud <itmechandu.ai@gmail.com>
2025-07-18 01:35:12 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to Digital Sai Chandu</h1>
+      <p>We provide Digital Marketing & AI Automation services.</p>
+    </div>
+  );
+}
